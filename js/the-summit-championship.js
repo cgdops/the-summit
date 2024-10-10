8593b54acd6e1596949c96e68e84ba81f2ea8914
bychrisgomez <christian@vantage-communications.com>
2024-10-10 14:04:01 -0400

    Update

--- js/the-summit-championship.js
+++ js/the-summit-championship.js
@@ -6100,6 +6100,9 @@
       };
       var getPluginDestination2 = (actionItemConfig) => actionItemConfig.value.inputs ?? {};
       var createPluginInstance3 = (element, actionItem) => {
+        const selectorGuids = actionItem.config?.target?.selectorGuids || [];
+        if (selectorGuids.length > 0)
+          return element;
         const pluginElementId = actionItem?.config?.target?.pluginElement;
         return pluginElementId ? queryContainerElement(pluginElementId) : null;
       };
@@ -10660,7 +10663,11 @@
       const actionGroups = instanceActionGroups[key];
       const actionItem = (0, import_get2.default)(actionGroups, `[0].actionItems[0]`, {});
       const { actionTypeId } = actionItem;
-      const pluginInstance = isPluginType2(actionTypeId) ? createPluginInstance2(actionTypeId)(element, actionItem) : null;
+      const shouldUsePlugin = (
+        // If it's targeted by class, don't query the element by pluginElementId
+        actionTypeId === ActionTypeConsts.PLUGIN_RIVE ? (actionItem.config?.target?.selectorGuids || []).length === 0 : isPluginType2(actionTypeId)
+      );
+      const pluginInstance = shouldUsePlugin ? createPluginInstance2(actionTypeId)(element, actionItem) : null;
       const destination = getDestinationValues2(
         { element, actionItem, elementApi: IX2BrowserApi_exports },
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -12167,6 +12174,19 @@
     "packages/shared/render/plugins/Form/webflow-forms.js"(exports, module) {
       "use strict";
       var Webflow = require_webflow_lib();
+      var renderTurnstileCaptcha = (siteKey, formElement, cb, errorCallback) => {
+        const captchaContainer = document.createElement("div");
+        formElement.appendChild(captchaContainer);
+        turnstile.render(captchaContainer, {
+          sitekey: siteKey,
+          callback: function(token) {
+            return cb(token);
+          },
+          "error-callback": function() {
+            errorCallback();
+          }
+        });
+      };
       Webflow.define("forms", module.exports = function($, _) {
         var api = {};
         var $doc = $(document);
@@ -12256,13 +12276,38 @@
         }
         function addListeners() {
           listening = true;
-          $doc.on("submit", namespace + " form", function(evt) {
-            var data = $.data(this, namespace);
-            if (data.handler) {
-              data.evt = evt;
-              data.handler(data);
-            }
-          });
+          const turnstileSiteKey = $doc.find("[data-turnstile-sitekey]").data("turnstile-sitekey");
+          if (turnstileSiteKey) {
+            const script = document.createElement("script");
+            script.src = "https://challenges.cloudflare.com/turnstile/v0/api.js";
+            document.head.appendChild(script);
+            script.onload = () => {
+              $doc.on("submit", namespace + " form", function(evt) {
+                var data = $.data(this, namespace);
+                disableBtn(data);
+                if (data.handler) {
+                  data.evt = evt;
+                  evt.preventDefault();
+                  renderTurnstileCaptcha(turnstileSiteKey, this, (turnstileToken) => data.handler({
+                    ...data,
+                    turnstileToken
+                  }), () => {
+                    data.fail.toggle(true);
+                    data.fail.focus();
+                    reset(data);
+                  });
+                }
+              });
+            };
+          } else {
+            $doc.on("submit", namespace + " form", function(evt) {
+              var data = $.data(this, namespace);
+              if (data.handler) {
+                data.evt = evt;
+                data.handler(data);
+              }
+            });
+          }
           const CHECKBOX_CLASS_NAME = ".w-checkbox-input";
           const RADIO_INPUT_CLASS_NAME = ".w-radio-input";
           const CHECKED_CLASS = "w--redirected-checked";
